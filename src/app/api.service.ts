f810ca937fd39c4325c80147d8685f8a19fb55cf
Nopesito <er.pino@duocuc.cl>
2023-09-29 12:21:08 -0300

    muerte a la api

---
+++ src/app/api.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { retry, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
+  // Se establece la base url del API a consumir
+  apiURL = 'http://localhost:3000';
+  // Se declara la variable http de tipo HttpClient
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiURL + '/users').pipe(
+      retry(3)
+    );
+  }
+
+  getPosts(): Observable<any> {
+    return this.http.get(this.apiURL + '/posts').pipe(
+      retry(3)
+    );
+  }
+
+  postPosts(post:any): Observable<any> {
+    return this.http.post(this.apiURL + '/posts', post, this.httpOptions).pipe(
+      retry(3)
+    );
+
+  }
+
+  deletePost(id:any): Observable<any> {
+    return this.http.delete(this.apiURL + '/posts/' + id, this.httpOptions)
+  }
+
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,22 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  user:any;
-  users:any;
-  posts:any;
-  post:any={
-    id:null,
-    title:"",
-    body:"",
-    userId:null
+  user: any;
+  users: any;
+  posts: any;
+  post: any = {
+    id: null,
+    title: "",
+    body: "",
+    userId: null
   };
-  
-  compareWith:any;
-  
-  constructor() {}
-  
-  compareWithFn = (o1:any, o2:any) => {
+
+  compareWith: any;
+
+  constructor(private api: ApiService) { }
+
+  compareWithFn = (o1: any, o2: any) => {
     return o1 && o2 ? o1.id === o2.id : o1 === o2;
   };
-  
+
+  guardarPost() {
+    this.post.userId = this.user
+    this.api.postPosts(this.post);
+    this.obtenerPosts();
+  };
+
+  eliminarPost(post:any) {
+    this.api.deletePost(post.id);
+  };
+
+  obtenerUsuarios(){
+    this.api.getUsers().subscribe((data)=>{
+      this.users = data;
+    })
+  }
+
+  setPost(){
+
+  }
+
+  obtenerPosts(){
+    this.api.getPosts().subscribe((res)=>{
+      this.posts = res;
+    })
+  }
+
+  ionViewWillEnter(){
+    this.obtenerUsuarios();
+    this.obtenerPosts();
+  }
+
 }
